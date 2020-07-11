8d9effb8c2fe651822319da398dea1298191d2c4
Joe Chavala Jr <jbac76@gmail.com>
2020-07-11 17:50:39 +0300

    initial commit

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function __construct()
+    {
+        // $this->middleware('guest')
+    }
+
+    public function index()
+    {
+        $data = ['name'=> 'Joe'];
+        return view('admin.index', compact('data'));
+    }
+}
