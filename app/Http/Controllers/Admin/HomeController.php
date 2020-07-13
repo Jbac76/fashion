4ba29239f0ba4ac48b867b151e1fece5879dd535
Joe Chavala Jr <jbac76@gmail.com>
2020-07-13 16:33:09 +0300

    Fixes the Admin controller namespace

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller\Admin;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
